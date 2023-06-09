f51db0f78600fddc10defa80b7e8d75ff5550b3d
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-06-09 14:04:28 -0700

    classname logic done

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -1,12 +1,35 @@
+import {useState} from 'react'
+
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
 
   return (
+    <>
     <input
       type="number"
       disabled={disabled}
       value={rating}
       onChange={onChange}
     />
+
+    <div className="rating-input">
+      <div className= {rating >=  1 ? "filled" : "empty" }>
+        <i className="fa fa-paw"/>
+      </div>
+      <div className={rating >=  2 ? "filled" : "empty" }>
+        <i className="fa fa-paw"/>
+      </div>
+      <div className={rating >=  3 ? "filled" : "empty" }>
+        <i className="fa fa-paw"/>
+      </div>
+      <div className={rating >=  4 ? "filled" : "empty" }>
+        <i className="fa fa-paw"/>
+      </div>
+      <div className={rating >=  5 ? "filled" : "empty" }>
+        <i className="fa fa-paw"/>
+      </div>
+      
+    </div>
+    </>
   );
 };
 

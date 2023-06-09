de493461aac92e8648c52470694f34d8721e31d9
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-06-09 14:55:27 -0700

    hovering

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -16,30 +16,30 @@ const PawsRatingInput = ({ rating, disabled, onChange }) => {
     <div className="rating-input">
       <div className= {activeRating >=  1 ? "filled" : "empty" }
 
-      onMouseEnter={disabled ? setActiveRating(rating) : () => setActiveRating(1)}
+      onMouseEnter={disabled ? () => activeRating : () => setActiveRating(1)}
       onMouseLeave={ () => setActiveRating(rating)}>
         <i className="fa fa-paw"/>
       </div>
       <div className={activeRating >=  2 ? "filled" : "empty" }
 
-      onMouseEnter={() => setActiveRating(2)}
+      onMouseEnter={disabled ? () => activeRating : () => setActiveRating(2)}
       onMouseLeave={() => setActiveRating(rating)}>
         <i className="fa fa-paw"/>
       </div>
       <div className={activeRating >=  3 ? "filled" : "empty" }
 
-      onMouseEnter={() => setActiveRating(3)}
+      onMouseEnter={disabled ? () => activeRating : () => setActiveRating(3)}
       onMouseLeave={() => setActiveRating(rating)}>
         <i className="fa fa-paw"/>
       </div>
       <div className={activeRating >=  4 ? "filled" : "empty" }
-      onMouseEnter={() => setActiveRating(4)}
+      onMouseEnter={disabled ? () => activeRating : () => setActiveRating(4)}
       onMouseLeave={() => setActiveRating(rating)}>
         <i className="fa fa-paw"/>
       </div>
       <div className={activeRating >=  5 ? "filled" : "empty" }
 
-      onMouseEnter={() => setActiveRating(5)}
+      onMouseEnter={disabled ? () => activeRating : () => setActiveRating(5)}
       onMouseLeave={() => setActiveRating(rating)}>
         <i className="fa fa-paw"/>
       </div>
